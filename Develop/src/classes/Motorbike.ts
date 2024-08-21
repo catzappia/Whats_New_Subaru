763e56fa55abdca994ee3934cd801f9117ac3e7c
Catherine Zappia <catzappia@aol.com>
2024-08-21 17:31:15 -0400

    changed format of the constructor and initialized the properties.

--- Develop/src/classes/Motorbike.ts
+++ Develop/src/classes/Motorbike.ts
@@ -21,8 +21,19 @@ class Motorbike extends Vehicle{
     // TODO: The constructor should call the constructor of the parent class, Vehicle
     // TODO: The constructor should initialize the properties of the Motorbike class
     // TODO: The constructor should check if the wheels array has 2 elements and create 2 new default Wheel objects if it does not
-  constructor(vin: string, color: string, make: string, model: string, year: number, weight: number, topSpeed: number, wheels: Wheel[]) {
-    super ();
+  constructor(
+    vin: string, 
+    color: string, 
+    make: string, 
+    model: string, 
+    year: number, 
+    weight: number, 
+    topSpeed: number, 
+    wheels: Wheel[]
+    ) {
+
+    super();
+
     this.vin = vin;
     this.color = color;
     this.make = make;
